995cb2c9ac3d61cb21002afce141ce6c5acb21c5
Xiaofeng <xfzhao1997@gmail.com>
2021-05-03 20:41:22 +0800

    update MVE_solver, Unittest

--- MVE_solver.py
+++ MVE_solver.py
@@ -6,12 +6,22 @@ Author: Xiaofeng Zhao
 
 import graph_tool as gt
 import graph_tool.stats as gt_st
+import graph_tool.topology as gt_topo
+import graph_tool.search as gt_sch
 import networkx as nx
 import parse
+import itertools
 
 # The path of input files. Modify the path if necessary
 input_path = "/home/xiaofeng/CS170/inputs"
 
+
+class Visitor_all_reachable_vertices(gt_sch.DFSVisitor):
+    def __init__(self, v_list: list):
+        self.v_list = v_list
+
+    def add_to_list(self):
+        self.v_list.append(self)
 
 def nx2gt(G_nx: nx.Graph):
     """
@@ -32,3 +42,96 @@ def nx2gt(G_nx: nx.Graph):
     g.edge_properties["weight"] = eprop
 
     return g
+
+
+def get_shortest_path_tree(g: gt.Graph, source):
+    """
+    Construct a shortest path tree from source
+    Args:
+        g: The graph
+        source:
+
+    Returns:
+        short_path_tree
+    """
+    shortest_path_tree = gt.Graph(directed=True)
+
+    dist_map, pred_map = gt_topo.shortest_distance(g, source, pred_map=True)
+
+    return shortest_path_tree
+
+
+def get_shortest_st_path(g: gt.Graph):
+    """ Return the shortest path from s to t. """
+    source = g.vertex(0)
+    target = g.vertex(g.num_vertices() - 1)
+    _, e_list = gt_topo.shortest_path(g, source, target)
+    return e_list
+
+
+def get_cut_from_SPT(g:gt.Graph, shortest_path_tree: gt.Graph, edge_to_delete):
+    """
+    After deleting <edge_to_delete>, we will have 2 trees. The 2 trees divide the node set of
+    graph <g> into two subsets. The function returns the cut that forms these two node subsets.
+    Args:
+        shortest_path_tree:
+        edge_to_delete:
+
+    Returns:
+
+    """
+    # Assuming the edge_to_delete is (u,v), v is the root of the second tree.
+    cut_list = []
+    v = edge_to_delete.source()
+    nodes_of_second_tree = set.union({v.target() for v in gt_sch.dfs_iterator(shortest_path_tree, v)},
+                                     {v.source() for v in gt_sch.dfs_iterator(shortest_path_tree, v)})
+    all_nodes = {v for v in g.vertices()}
+    nodes_of_first_tree = all_nodes.difference(nodes_of_second_tree)
+
+    for n_1 in nodes_of_first_tree:
+        for n_2 in nodes_of_second_tree:
+            cut = g.edge(n_1, n_2)
+            if cut is not None:
+                cut_list.append(cut)
+
+    return cut_list
+
+
+def solver(g, k, c):
+    """
+    MVE_solver
+    Args:
+        g: The graph where we delete edges and vertices
+        k: The maximum number of edges to delete
+        c: The maximum number of nodes to delete
+
+    Returns:
+        edges_to_delete: a list of edges to be removed
+    """
+
+
+""" The following are abandoned methods """
+def get_shortest_path_tree_v1(g: gt.Graph, source):
+    """
+    Construct a shortest path tree from source
+    Args:
+        g: The graph
+        source:
+
+    Returns:
+        short_path_tree
+    """
+    shortest_path_tree = gt.Graph(directed=True)
+
+    target_list = [v for v in g.vertices()]
+    target_list.remove(g.vertex(0))
+
+    SPT_edges = set()
+    for v in target_list:
+        _, e_list = gt_topo.shortest_path(g, source, v)
+        SPT_edges = SPT_edges.union(set(e_list))
+    shortest_path_tree.add_edge_list(SPT_edges)
+
+    shortest_path_tree.set_reversed(is_reversed=True)
+
+    return shortest_path_tree
--- Unittest.py
+++ Unittest.py
@@ -4,9 +4,22 @@ import os
 import networkx as nx
 import MVE_solver
 import graph_tool.topology as gt_topo
+import graph_tool as gt
+import graph_tool.draw as gt_draw
 
 
 class MyTestCase(unittest.TestCase):
+    def get_graph_sample(self):
+
+        # Modify the idx if you want to try another sample
+        idx = 0
+        # parse the sample
+        small_input_path = os.getcwd() + "/inputs" + "/small"
+        inputs = os.listdir(small_input_path)
+        input = small_input_path + "/" + inputs[idx]
+        g_nx = parse.read_input_file(input)
+        g = MVE_solver.nx2gt(g_nx)
+        return g
 
     def test_get_nx(self):
         small_input_path = os.getcwd() + "/inputs" + "/small"
@@ -18,7 +31,54 @@ class MyTestCase(unittest.TestCase):
         print(g.edge_properties)
         v_list, e_list = gt_topo.shortest_path(g, g.vertex(0), g.vertex(13), weights=g.edge_properties['weight'])
         print([str(v) for v in v_list])
+        print([str(e) for e in e_list])
 
+    def test_add_edge_list(self):
+        small_input_path = os.getcwd() + "/inputs" + "/small"
+        inputs = os.listdir(small_input_path)
+        input = small_input_path + "/" + inputs[0]
+        g_nx = parse.read_input_file(input)
+        g = MVE_solver.nx2gt(g_nx)
+        v_list, e_list = gt_topo.shortest_path(g, g.vertex(0), g.vertex(13), weights=g.edge_properties['weight'])
+
+        new_g = gt.Graph()
+        new_g.add_vertex(g.num_vertices())
+        new_g.add_edge_list(set(e_list))
+
+        for e in new_g.edges():
+            print(e)
+            a = list(e)[1]
+            b = e
+
+    def test_cut(self):
+        g = self.get_graph_sample()
+
+        st_path = MVE_solver.get_shortest_st_path(g)
+        print([e for e in st_path])
+
+    def test_draw_graph(self):
+        g = self.get_graph_sample()
+        spt = MVE_solver.get_shortest_path_tree(g, g.vertex(0))
+        gt_draw.graph_draw(spt, vertex_text=spt.vertex_index)
+
+    def test_get_cut(self):
+        g = self.get_graph_sample()
+        st_path = MVE_solver.get_shortest_st_path(g)
+        spt = MVE_solver.get_shortest_path_tree(g, g.vertex(0))
+
+        e = st_path[0]
+        cuts = MVE_solver.get_cut_from_SPT(g, spt, e)
+        print([e for e in cuts])
+
+    def test_short_distance(self):
+        g = self.get_graph_sample()
+        dist_map, pred_map = gt_topo.shortest_distance(g, g.vertex(0), weights=g.edge_properties['weight'], pred_map=True)
+
+        print(dist_map.a)
+        print(pred_map.a)
+        list = [x for x in pred_map.a]
+
+        a = list
 
 if __name__ == '__main__':
     unittest.main()
